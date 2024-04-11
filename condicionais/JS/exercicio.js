7b71dce5a2d5842da010e1ff71f9b7b51b1e11d6
Vtdasul1 <victor.e.silva@aluno.senai.br>
2024-04-11 10:43:55 -0300

    Atividade JS

---
+++ condicionais/JS/exercicio.js
@@ -0,0 +1,4 @@
+var vel = 60.5
+console.log(`a velocidade do seu carro é ${vel}km/hr` )
+console.log('dirija sempre usando o cinto de segurança')
+console.log('Você ultrapassou o velocidade permitida, MULTADO!')
---
+++ condicionais/JS/exercicio1.js
@@ -0,0 +1,8 @@
+var vel = 80
+console.log(`a velocidade do seu carro é ${vel}km/hr` )
+if(vel>60){
+    console.log('Você ultrapassou o velocidade permitida, MULTADO!')
+
+}
+console.log('dirija sempre usando o cinto de segurança')
+
